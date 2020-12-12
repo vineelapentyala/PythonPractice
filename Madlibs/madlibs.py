78de8fa14c5637e2919ef2a8a538c3e7f1ddfeff
vineelapentyala <pentyala@seas.upenn.edu>
2020-12-11 22:53:51 -0500

    madlibs

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,10 @@
+# Get Input from user by specifying the 'part of speech' 
+# for the word you are looking for
+
+noun = input("Noun: ")
+pronoun = input("ProNoun: ")
+verb = input("Verb: ")
+
+madlib = "Hi, " + noun + " is my best friend. " + pronoun + " is a doctor by the day but " + verb + " after work." 
+
+print(madlib)
